9b3aa41caaa1fbfdb613d704f5dd115712388d8a
Eridian <86061486+EridianAlpha@users.noreply.github.com>
2024-09-10 19:35:55 +0100

    Use IPFS direct link

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -7,8 +7,7 @@ import {SettlementNft} from "src/SettlementNft.sol";
 
 contract Deploy is Script {
     function run() public returns (address deployedSettlementNftAddress, address deployedSettlerTokenAddress) {
-        // TODO: Change to a native IPFS URI and manage display on the UI side
-        string memory baseImageUri = "https://ipfs.io/ipfs/QmViprAUrkh1ECEYBnXyNgDhcij6giT6xJuPkNCk8pTjbX";
+        string memory baseImageUri = "ipfs://QmViprAUrkh1ECEYBnXyNgDhcij6giT6xJuPkNCk8pTjbX";
 
         vm.startBroadcast(msg.sender);
         deployedSettlementNftAddress = address(new SettlementNft(baseImageUri));
