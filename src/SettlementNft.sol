ce9644600529133f673a4ef7f9ad6f8014ea685a
Eridian <86061486+EridianAlpha@users.noreply.github.com>
2024-09-12 14:21:10 +0100

    Split SVG format

--- src/SettlementNft.sol
+++ src/SettlementNft.sol
@@ -12,17 +12,17 @@ contract SettlementNft is ERC721 {
     error SettlementNft_SingleActivePerAddress();
 
     // State variables
-    SettlerToken public immutable i_settlerToken;
-    string public baseImageUri;
+    SettlerToken public immutable SETTLER_TOKEN;
+    string public BASE_IMAGE_URI;
     uint256 public nextTokenId = 1; // Start token IDs at 1
-    mapping(uint256 => uint256) public mintTimestamp;
-    mapping(address => uint256) public ownerToId;
+    mapping(uint256 => uint256) public s_mintTimestamp;
+    mapping(address => uint256) public s_ownerToId;
 
     constructor(string memory _baseImageUri) ERC721("Ethereum Settlement", "SETTLEMENT") {
-        baseImageUri = _baseImageUri;
+        BASE_IMAGE_URI = _baseImageUri;
 
         // Deploy the SETTLER ERC20 token
-        i_settlerToken = new SettlerToken(address(this));
+        SETTLER_TOKEN = new SettlerToken(address(this));
     }
 
     function _update(address to, uint256 tokenId, address auth) internal override returns (address) {
@@ -30,38 +30,24 @@ contract SettlementNft is ERC721 {
         require(balanceOf(msg.sender) == 0, SettlementNft_SingleActivePerAddress());
 
         // Mint any outstanding SETTLER tokens from the SettlerToken contract
-        i_settlerToken.mintOutstandingTokensFromNft(msg.sender);
+        SETTLER_TOKEN.mintOutstandingTokensFromNft(msg.sender);
 
-        // Update the ownerToId mapping
-        ownerToId[msg.sender] = 0;
-        ownerToId[to] = tokenId;
+        // Update the s_ownerToId mapping
+        s_ownerToId[msg.sender] = 0;
+        s_ownerToId[to] = tokenId;
 
+        // TODO: Which address is returned?
         return super._update(to, tokenId, auth);
     }
 
     function mint() external {
         _safeMint(msg.sender, nextTokenId);
-        mintTimestamp[nextTokenId] = block.timestamp;
-        ownerToId[msg.sender] = nextTokenId;
+        s_mintTimestamp[nextTokenId] = block.timestamp;
+        s_ownerToId[msg.sender] = nextTokenId;
         nextTokenId++;
     }
 
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        // Create the custom SVG with the timestamp as an overlay
-        string memory svg = string(
-            abi.encodePacked(
-                '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 350 350">',
-                '<image href="',
-                baseImageUri,
-                '" height="100%" width="100%" />',
-                '<rect x="25%" y="32%" rx="20" ry="20" width="50%" height="38" fill="#201649" />',
-                '<text x="50%" y="38%" dominant-baseline="middle" text-anchor="middle" font-size="24" font-weight="bold" font-family="monospace" fill="white">',
-                Strings.toString(((block.timestamp + (60 * 60 * 24 * 1234)) - mintTimestamp[tokenId]) / (60 * 60 * 24)),
-                " days</text>",
-                "</svg>"
-            )
-        );
-
         // Create the JSON metadata and encode it in base64
         string memory json = Base64.encode(
             bytes(
@@ -72,21 +58,74 @@ contract SettlementNft is ERC721 {
                         '",',
                         '"description": "An on-chain NFT representing an Ethereum settlement.",',
                         '"image": "',
-                        baseImageUri,
+                        BASE_IMAGE_URI,
                         '",',
                         '"imageCompositeSvg": "data:image/svg+xml;base64,',
-                        Base64.encode(bytes(svg)),
+                        Base64.encode(bytes(_generateSvg(tokenId))),
                         '",',
                         '"attributes": [',
                         '{"trait_type": "Mint Timestamp", "value": "',
-                        Strings.toString(mintTimestamp[tokenId]),
+                        Strings.toString(s_mintTimestamp[tokenId]),
                         '"}',
                         "]}"
                     )
                 )
             )
         );
-
         return string(abi.encodePacked("data:application/json;base64,", json));
+    }
+
+    function _generateSvg(uint256 tokenId) internal view returns (string memory svg) {
+        string memory tokenIdString = Strings.toString(tokenId);
+
+        // Create the custom SVG with the timestamp as an overlay
+        svg = string(
+            abi.encodePacked(
+                '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 350 350">',
+                '<image href="',
+                BASE_IMAGE_URI,
+                '" height="100%" width="100%" />',
+                _generateSettlementRectangle(tokenIdString),
+                _generateDaysSinceMintRectangle(tokenId),
+                "</svg>"
+            )
+        );
+    }
+
+    function _generateSettlementRectangle(string memory tokenIdString) internal pure returns (string memory) {
+        uint256 settlementNumberXWidth = 55 + ((bytes(tokenIdString).length) * 5);
+
+        return string(
+            abi.encodePacked(
+                '<rect x="',
+                Strings.toString((100 - settlementNumberXWidth) / 2),
+                '%" y="30%" rx="20" ry="20" width="',
+                Strings.toString(settlementNumberXWidth),
+                '%" height="38" fill="#201649" />',
+                '<text x="50%" y="36%" dominant-baseline="middle" text-anchor="middle" font-size="24" font-weight="bold" font-family="monospace" fill="white">',
+                "Settlement #",
+                tokenIdString,
+                "</text>"
+            )
+        );
+    }
+
+    function _generateDaysSinceMintRectangle(uint256 tokenId) internal view returns (string memory) {
+        uint256 daysSinceMint = ((block.timestamp + (60 * 60 * 24 * 1234)) - s_mintTimestamp[tokenId]) / (60 * 60 * 24);
+        uint256 daysSinceMintXWidth = 22 + ((bytes(Strings.toString(daysSinceMint)).length) * 5);
+
+        return string(
+            abi.encodePacked(
+                '<rect x="',
+                Strings.toString((100 - daysSinceMintXWidth) / 2),
+                '%" y="42%" rx="20" ry="20" width="',
+                Strings.toString(daysSinceMintXWidth),
+                '%" height="38" fill="#201649" />',
+                '<text x="50%" y="48%" dominant-baseline="middle" text-anchor="middle" font-size="24" font-weight="bold" font-family="monospace" fill="white">',
+                Strings.toString(daysSinceMint > 1 ? daysSinceMint : 1),
+                daysSinceMint > 1 ? " days" : " day",
+                "</text>"
+            )
+        );
     }
 }
